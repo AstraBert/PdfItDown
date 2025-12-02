208bcf5215dbf0f9565cca797d7a2a62aec06f62
Clelia (Astra) Bertelli <clelia@runllama.ai>
2025-12-02 11:36:31 +0100

    feat: switch to typer for CLI app

--- src/pdfitdown/cli/app.py
+++ src/pdfitdown/cli/app.py
@@ -1,140 +1,111 @@
-import click
+from typer import Typer, Option, Exit
+from typing import Annotated, cast
+from rich import print as rprint
 from pathlib import Path
-from click.exceptions import Exit
-from termcolor import cprint
-from typing import cast
 from ..pdfconversion.converter import Converter
 
+app = Typer()
 
-@click.command(
-    help="Convert (almost) everything to PDF",
-)
-@click.option(
-    "-i",
-    "--inputfile",
-    help="Path to the input file(s) that need to be converted to PDF. Can be used multiple times.",
-    required=False,
-    default=None,
-    multiple=True,
-)
-@click.option(
-    "-o",
-    "--outputfile",
-    help="Path to the output PDF file(s). If more than one input file is provided, you should provide an equal number of output files.",
-    required=False,
-    default=None,
-    multiple=True,
-)
-@click.option(
-    "-t",
-    "--title",
-    help="Title to include in the PDF metadata. Default: 'File Converted with PdfItDown'. If more than one file is provided, it will be ignored.",
-    required=False,
-    default=None,
-)
-@click.option(
-    "-d",
-    "--directory",
-    help="Directory whose files you want to bulk-convert to PDF. If `--inputfile` is also provided, this option will be ignored. Defaults to None.",
-    required=False,
-    default=None,
-)
-def pdfitdown_cli(
-    inputfile: tuple[str, ...] | None,
-    outputfile: tuple[str, ...] | None,
-    title: str | None,
-    directory: str | None,
+@app.command()
+def main(
+    inputfile: Annotated[
+        list[str], 
+        Option(
+            "--inputfile", 
+            "-i", 
+            help="Path to the input file(s) that need to be converted to PDF. Can be used multiple times."
+        )
+    ] = [],
+    outputfile: Annotated[
+        list[str], 
+        Option(
+            "--outputfile", 
+            "-o", 
+            help="Path to the output PDF file(s). If more than one input file is provided, you should provide an equal number of output files."
+        )
+    ] = [],
+    title: Annotated[
+        str | None,
+        Option(
+             "-t",
+            "--title",
+            help="Title to include in the PDF metadata. Default: 'File Converted with PdfItDown'. If more than one file is provided, it will be ignored.",
+        )
+    ] = None,
+    directory: Annotated[
+        str | None,
+        Option(
+            "-d",
+            "--directory",
+            help="Directory whose files you want to bulk-convert to PDF. If `--inputfile` is also provided, this option will be ignored. Defaults to None.",
+        )
+    ] = None
 ):
     c = Converter()
-    if (
-        inputfile is None or (inputfile is not None and len(inputfile) == 0)
-    ) and directory is None:
-        cprint(
-            "ERROR! You should provide one of `--inputfile` or `--directory`",
-            attrs=["bold"],
-            color="red",
+    if len(inputfile) == 0 and directory is None:
+        rprint(
+            "[bold red]ERROR! You should provide one of `--inputfile` or `--directory`[/]",
         )
         raise Exit(1)
-    elif inputfile is not None and len(inputfile) > 0:
+    elif len(inputfile) > 0:
         if directory is not None:
-            cprint(
-                "WARNING: `--directory` will be ignored since `--inputfile` has been provided",
-                attrs=["bold"],
-                color="yellow",
+            rprint(
+                "[bold yellow]WARNING: `--directory` will be ignored since `--inputfile` has been provided[/]",
             )
         if len(inputfile) == 1:
-            if outputfile is None or len(outputfile) == 0:
-                outputfile = (inputfile[0].replace(Path(inputfile[0]).suffix, ".pdf"),)
+            if len(outputfile) == 0:
+                outputfile = [inputfile[0].replace(Path(inputfile[0]).suffix, ".pdf")]
             try:
                 c.convert(inputfile[0], outputfile[0], title, True)
             except Exception as e:
-                cprint(
-                    f"ERROR during the conversion: {e}",
-                    attrs=["bold"],
-                    color="red",
+                rprint(
+                    f"[bold red]ERROR during the conversion: {e}[/]",
                 )
                 raise Exit(2)
-            cprint(
-                "Conversion successful!🎉",
-                color="green",
-                attrs=["bold"],
+            rprint(
+                "[bold green]Conversion successful![/]🎉",
             )
         else:
             if title is not None:
-                cprint(
-                    "WARNING: `--title` will be ignored since more than one `--inputfile` has been provided",
-                    attrs=["bold"],
-                    color="yellow",
+                rprint(
+                    "[bold yellow]WARNING: `--title` will be ignored since more than one `--inputfile` has been provided[/]",
                 )
             outputfile_ls = None
-            if outputfile is not None and len(outputfile) > 0:
-                outputfile_ls = list(outputfile)
+            if len(outputfile) > 0:
+                outputfile_ls = outputfile
             if outputfile_ls is not None and len(outputfile_ls) != len(inputfile):
-                cprint(
-                    "ERROR! `--inputfile` and `--outputfile` should be the same number",
-                    attrs=["bold"],
-                    color="red",
+                rprint(
+                    "[bold red]ERROR! `--inputfile` and `--outputfile` should be the same number[/]",
                 )
                 raise Exit(1)
             try:
                 c.multiple_convert(list(inputfile), outputfile_ls)
             except Exception as e:
-                cprint(
-                    f"ERROR during the conversion: {e}",
-                    attrs=["bold"],
-                    color="red",
+                rprint(
+                    f"[bold red]ERROR during the conversion: {e}[/]",
                 )
                 raise Exit(2)
-            cprint(
+            rprint(
                 "Conversion successful!🎉",
-                color="green",
-                attrs=["bold"],
             )
     else:
         directory = cast(str, directory)
-        if outputfile is not None and len(outputfile) > 0:
-            cprint(
-                "WARNING: `--outputfile` will be ignored since  `--inputfile` has not been provided",
-                attrs=["bold"],
-                color="yellow",
+        if len(outputfile) > 0:
+            rprint(
+                "[bold yellow]WARNING: `--outputfile` will be ignored since  `--inputfile` has not been provided[/]",
             )
         if title is not None:
-            cprint(
-                "WARNING: `--title` will be ignored since `--directory` has been provided",
-                attrs=["bold"],
-                color="yellow",
+            rprint(
+                "[bold yellow]WARNING: `--title` will be ignored since `--directory` has been provided[/]",
             )
         try:
             c.convert_directory(directory)
         except Exception as e:
-            cprint(
-                f"ERROR during the conversion: {e}",
-                attrs=["bold"],
-                color="red",
+            rprint(
+                f"[bold red]ERROR during the conversion: {e}",
             )
             raise Exit(2)
-        cprint(
-            "Conversion successful!🎉",
-            color="green",
-            attrs=["bold"],
+        rprint(
+            "[bold green]Conversion successful![/]🎉",
         )
+    
